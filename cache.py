d643df33a3cbe7ca09d900af1453ff244189f8f0
crapthings <crapthings@gmail.com>
2023-10-27 18:22:06 +0800

    fix cache

--- cache.py
+++ cache.py
@@ -4,4 +4,4 @@ from diffusers import StableDiffusionPipeline, AutoPipelineForImage2Image, Stabl
 model1 = './dreamshaper_8.safetensors'
 txt2imgPipe = StableDiffusionPipeline.from_single_file(model1, torch_dtype = torch.float16, safety_checker = None)
 AutoPipelineForImage2Image.from_pipe(txt2imgPipe, safety_checker = None)
-StableDiffusionUpscalePipeline.from_pretrained('stabilityai/stable-diffusion-x4-upscaler', revision = 'fp16', torch_dtype = torch.float16).to('cuda')
+StableDiffusionUpscalePipeline.from_pretrained('stabilityai/stable-diffusion-x4-upscaler', revision = 'fp16', torch_dtype = torch.float16)
